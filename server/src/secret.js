c0291d12f63e6afeb8a278507fa65f87ca182fc2
shahriartamim2 <abdnoman1616@gmail.com>
2024-07-27 10:15:33 +0600

    chore: Add user Schema ,  bcrypt dependency and default user image path

--- server/src/secret.js
+++ server/src/secret.js
@@ -4,5 +4,6 @@ const serverPort = process.env.SERVER_PORT || 5001;
 const mongoUrl =
   process.env.MONGO_URL ||
   "mongodb+srv://shahriartamim:shahriartamim@ecommerce.mvpegea.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce";
+const defaultUserImage = process.env.DEFAULT_USER_IMAGE || "../public/images/users/default.png";
 
-export {serverPort, mongoUrl};
+export {serverPort, mongoUrl,defaultUserImage};
