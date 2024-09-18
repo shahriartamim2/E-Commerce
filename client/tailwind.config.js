6dfdf50d95d3f46e24e141a522632ed961fe10bc
shahriartamim2 <202011039@ye.butex.edu.bd>
2024-09-18 06:16:13 +0600

    updated - bgColor

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       colors: { // 'color' should be 'colors'
-        bgColor: ' #ffffcc', // Use hex or valid CSS color names
+        bgColor: ' #f2f2f2', // Use hex or valid CSS color names
         cardColor: '#f2f2f2',
         primaryColor: 'white', // LeafyGreen color as hex (or use 'green' for Tailwind's green)
       },
